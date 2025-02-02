944c464f0f0aacb01875c5cb92801b04be2cd44e
wilmaralfaro <wilmar.alfaro@techietalent.net>
2025-02-02 12:25:35 -0500

    chore: remove unused files

--- src/domain/use-cases/absenceRequest.usecase.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from "@/services/user.service";
-
-export class CreateAbsenceRequestUseCase {
-    constructor(private readonly userService: UserService){}
-
-    public static execute(from: string, to: string, type: string, evidencePath: string, comment: string, requestedById: string) {
-        return UserService.createAbsenceRequest(from, to, type, evidencePath, comment, requestedById)
-    }
-}
