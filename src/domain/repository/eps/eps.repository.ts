b52b22a49f2e4afe277ca192898982e4cf46ed06
wilmaralfaro <wilmar.alfaro@techietalent.net>
2025-02-03 13:47:48 -0500

    added EPS model, repository interfaces, and implementation for EPS management

---
+++ src/domain/repository/eps/eps.repository.ts
@@ -0,0 +1,9 @@
+import type { Eps } from "@/domain/Interfaces/Eps/eps.interface";
+
+export abstract class EpsRepository {
+    abstract getEps(): Promise<Eps[]>;
+    abstract getEpsById(id: number): Promise<Eps>;
+    abstract createEps(data: Eps): Promise<Eps>;
+    abstract updateEps(id: number, data: Eps): Promise<Eps>;
+    abstract deleteEps(id: number): Promise<Eps>;
+}
