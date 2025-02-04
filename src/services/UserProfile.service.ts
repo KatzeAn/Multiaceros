99b1ff312cc41fcab02cb658fe386a39fe4f2624
wilmaralfaro <wilmar.alfaro@techietalent.net>
2025-02-04 10:33:41 -0500

    refactor: remove unused user profile and birthdays use cases and services

--- src/services/UserProfile.service.ts
+++
@@ -1,9 +0,0 @@
-import { UserProfileModel } from "@/database/UserProfile/UserProfile.model";
-import type { UserProfile } from "@/domain/Interfaces/user/UserProfile.interface";
-
-export class UserProfileService {
-    public static  getUserProfile(userId: string): Promise<UserProfile> 
-    {
-        return new UserProfileModel().getUserProfile(userId);
-    }
-}
--- src/services/birtdays.service.ts
+++
@@ -1,7 +0,0 @@
-import { UserModel } from "@/database/user/user.model";
-
-export class BirthdaysService {
-    public static getBirthdaysOfMonths(): Promise<UserBirthdayList> {
-        return new UserModel().getBirthdaysOfMonth();
-    }
-}
