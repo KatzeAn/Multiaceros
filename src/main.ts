b0be73084dfb2173c727ab3064c8ca7f6b39d07c
Angela <angela.alfaro1033@gmail.com>
2025-02-04 11:03:11 -0500

    sidebar changes

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App.vue'
 import router from './presentation/router'
 
