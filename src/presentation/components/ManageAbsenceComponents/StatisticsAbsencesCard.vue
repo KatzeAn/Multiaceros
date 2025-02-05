2ec105ecde2a6c9d1af21bc508e14ba1cbbd171a
Angela <angela.alfaro1033@gmail.com>
2025-02-05 18:36:50 -0500

    informacion en la tabla

--- src/presentation/components/ManageAbsenceComponents/StatisticsAbsencesCard.vue
+++ src/presentation/components/ManageAbsenceComponents/StatisticsAbsencesCard.vue
@@ -1,93 +1,97 @@
 <template>
- <el-row class="" :gutter="16" >
-    <el-col :span="24">
-      <span class="font-bold text-2xl ">Estadística mensual</span>
-    </el-col>
-    <el-col 
-    :xs="24" 
-    :sm="12" 
-    :md="8" 
-    :lg="8"
-    class="mb-4 sm:mb-0">
-      <div class="statistic-card bg-[var(--success-alt-color)]">
-        <el-statistic :value="0">
-          <template #title>
-            <div style="display: inline-flex; align-items: center">
-              Ausencias Aprobadas
-              <el-tooltip
-                effect="dark"
-                content="Número de ausencias aprobadas este mes"
-                placement="top"
-              >
-                <el-icon style="margin-left: 4px" :size="12">
-                  <Warning />
-                </el-icon>
-              </el-tooltip>
-            </div>
-          </template>
-        </el-statistic>
-      </div>
-    </el-col>
-    <el-col 
-    :xs="24" 
-    :sm="12" 
-    :md="8" 
-    :lg="8"
-    class="mb-4 sm:mb-0">
-      <div class="statistic-card bg-[var(--primary-alt-color)]">
-        <el-statistic :value="0">
-          <template #title>
-            <div style="display: inline-flex; align-items: center">
-              Ausencias Rechazadas
-              <el-tooltip
-                effect="dark"
-                content="Número de ausencias rechazadas este mes"
-                placement="top"
-              >
-                <el-icon style="margin-left: 4px" :size="12">
-                  <Warning />
-                </el-icon>
-              </el-tooltip>
-            </div>
-          </template>
-        </el-statistic>
-      </div>
-    </el-col>
-    <el-col 
-    :xs="24" 
-    :sm="12" 
-    :md="8" 
-    :lg="8"
-    class="mb-4 sm:mb-0">
-      <div class="statistic-card bg-[var(--info-alt-color)]">
-        <el-statistic :value="0">
-          <template #title>
-            <div style="display: inline-flex; align-items: center">
-              Total de Ausencias
-              <el-tooltip
-                effect="dark"
-                content="Total de ausencias creadas este mes"
-                placement="top"
-              >
-                <el-icon style="margin-left: 4px" :size="12">
-                  <Warning />
-                </el-icon>
-              </el-tooltip>
-            </div>
-          </template>
-        </el-statistic>
-      </div>
-    </el-col>
-  </el-row> 
+  <el-row class="" :gutter="16" >
+     <el-col :span="24">
+       <span class="font-bold text-2xl ">Estadística mensual</span>
+     </el-col>
+     <el-col 
+     :xs="24" 
+     :sm="12" 
+     :md="8" 
+     :lg="8"
+     class="mb-4 sm:mb-0">
+       <div class="statistic-card bg-[var(--success-alt-color)]">
+         <el-statistic :value="0">
+           <template #title>
+             <div style="display: inline-flex; align-items: center">
+               Ausencias Aprobadas
+               <el-tooltip
+                 effect="dark"
+                 content="Número de ausencias aprobadas este mes"
+                 placement="top"
+               >
+                 <el-icon style="margin-left: 4px" :size="12">
+                   <Warning />
+                 </el-icon>
+               </el-tooltip>
+             </div>
+           </template>
+         </el-statistic>
+       </div>
+     </el-col>
+     <el-col 
+     :xs="24" 
+     :sm="12" 
+     :md="8" 
+     :lg="8"
+     class="mb-4 sm:mb-0">
+       <div class="statistic-card bg-[var(--primary-alt-color)]">
+         <el-statistic :value="0">
+           <template #title>
+             <div style="display: inline-flex; align-items: center">
+               Ausencias Rechazadas
+               <el-tooltip
+                 effect="dark"
+                 content="Número de ausencias rechazadas este mes"
+                 placement="top"
+               >
+                 <el-icon style="margin-left: 4px" :size="12">
+                   <Warning />
+                 </el-icon>
+               </el-tooltip>
+             </div>
+           </template>
+         </el-statistic>
+       </div>
+     </el-col>
+     <el-col 
+     :xs="24" 
+     :sm="12" 
+     :md="8" 
+     :lg="8"
+     class="mb-4 sm:mb-0">
+       <div class="statistic-card bg-[var(--info-alt-color)]">
+         <el-statistic :value="0">
+           <template #title>
+             <div style="display: inline-flex; align-items: center">
+               Total de Ausencias
+               <el-tooltip
+                 effect="dark"
+                 content="Total de ausencias creadas este mes"
+                 placement="top"
+               >
+                 <el-icon style="margin-left: 4px" :size="12">
+                   <Warning />
+                 </el-icon>
+               </el-tooltip>
+             </div>
+           </template>
+         </el-statistic>
+       </div>
+     </el-col>
+   </el-row> 
   <el-table :data="tableData" class="custom-table">
-    <el-table-column prop="name" label="Nombre" width="150" />
-    <el-table-column prop="from" label="Desde" width="180" />
-    <el-table-column prop="to" label="Hasta" width="180" />
-    <el-table-column prop="totalDays" label="Total de Días" width="120" />
-    <el-table-column prop="type" label="Tipo" width="150" />
+    <el-table-column prop="fullName" label="Nombre" width="150" />
+    <el-table-column prop="startDate" label="Desde" width="180" />
+    <el-table-column prop="endDate" label="Hasta" width="180" />
+    <el-table-column label="Total de Días" width="120">
+      <template #default="scope">
+        <span>{{ getTotalDays(scope.row.startDate, scope.row.endDate) }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="absenceType" label="Tipo" width="150" />
     <el-table-column prop="status" label="Estado" width="150">
       <template #default="scope">
-        <el-tag :type="getStatusType(scope.row.status)">{{ scope.row.status }}</el-tag>
+        <el-tag :type="getStatusType(scope.row.status)">{{ scope.row.comment }}</el-tag>
       </template>
     </el-table-column>
     <el-table-column label="Acciones" width="200">
@@ -103,46 +107,34 @@
       </template>
     </el-table-column>
   </el-table>
-
 </template>
 
-
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useAbsenceStore } from '@/presentation/stores/absence.store';
 
-const tableData = ref<Absence[]>([
-  { id: 1, name: 'Tom', from: '2016-05-03', to: '2016-05-03', totalDays: 1, type: 'Vacaciones', status: 'Pendiente' },
-  { id: 2, name: 'Alice', from: '2016-05-03', to: '2016-05-03', totalDays: 2, type: 'Enfermedad', status: 'Aprobado' },
-  { id: 3, name: 'Bob', from: '2016-05-03', to: '2016-05-03', totalDays: 3, type: 'Permiso', status: 'Cancelado' },
-  { id: 4, name: 'Tom', from: '2016-05-03', to: '2016-05-03', totalDays: 1, type: 'Vacaciones', status: 'Pendiente' },
-  { id: 5, name: 'Alice', from: '2016-05-03', to: '2016-05-03', totalDays: 2, type: 'Enfermedad', status: 'Aprobado' },
-  { id: 6, name: 'Bob', from: '2016-05-03', to: '2016-05-03', totalDays: 3, type: 'Permiso', status: 'Cancelado' },
-  { id: 7, name: 'Tom', from: '2016-05-03', to: '2016-05-03', totalDays: 1, type: 'Vacaciones', status: 'Pendiente' },
-  { id: 8, name: 'Alice', from: '2016-05-03', to: '2016-05-03', totalDays: 2, type: 'Enfermedad', status: 'Aprobado' },
-  { id: 9, name: 'Bob', from: '2016-05-03', to: '2016-05-03', totalDays: 3, type: 'Permiso', status: 'Cancelado' },
-]);
+const tableData = ref<Absence[]>([]);
 
-const getStatusType = (status: string) => {
+const getStatusType = (status: number) => {
   switch (status) {
-    case 'Aprobado': return 'success';
-    case 'Pendiente': return 'warning';
-    case 'Cancelado': return 'danger';
+    case 1: return 'success'; 
+    case 2: return 'warning';
+    case 3: return 'danger';
     default: return '';
   }
 };
 
 const absenceStore = useAbsenceStore();
 
-interface Absence {
-  id: number;
-  name: string;
-  from: string;
-  to: string;
-  totalDays: number;
-  type: string;
-  status: string;
-}
+const fetchAbsences = async () => {
+  const result = await absenceStore.fetchPendingAbsences();
+  console.log(result.absenceList);
+  tableData.value = result.absenceList;
+};
+
+onMounted(() => {
+  fetchAbsences();
+});
 
 const approveAbsence = (absenceId: number) => {
   absenceStore.approveAbsence(absenceId);
@@ -151,9 +143,21 @@ const approveAbsence = (absenceId: number) => {
 const rejectAbsence = (absenceId: number) => {
   absenceStore.rejectAbsence(absenceId);
 };
+
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString();
+};
+
+const getTotalDays = (startDate: string, endDate: string) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const difference = Math.floor((end.getTime() - start.getTime()) / (1000 * 3600 * 24));
+  return difference + 1; 
+};
 </script>
 
-  
+
   <style scoped>
   .el-statistic {
     --el-statistic-content-font-size: 28px;
