477a37ff2abc941b10ee53c7306f292ac9400af7
wilmaralfaro <wilmar.alfaro@techietalent.net>
2025-02-10 21:09:29 -0500

    refactor: implement family compensation fund management view with form validation, loading state, and pagination

---
+++ src/presentation/viewmodels/familyCompensationFundViewModel.ts
@@ -0,0 +1,113 @@
+import { ref, computed, reactive, watch, onMounted } from "vue";
+import { useFamilyCompensationFundStore } from "../stores/familyCompensationFund.store";
+import { ElNotification, type FormInstance } from "element-plus";
+import type { FamilyCompesationFunds } from "@/domain/Interfaces/FamilyCompesationFunds/FamilyCompesationFunds.interface";
+
+export function useFamilyCompensationFundViewModel() {
+  const familyCompensationFundStore = useFamilyCompensationFundStore();
+  const familyCompensationFundList = ref<FamilyCompesationFunds[]>([]);
+  const isLoading = computed(() => familyCompensationFundStore.isLoading);
+
+  const search = ref("");
+  const currentPage = ref(1);
+  const pageSize = ref(10);
+  const ruleFormRef = ref<FormInstance>();
+
+  const familyCompensationFundForm = reactive<FamilyCompesationFunds>({
+    compensationFundName: "",
+  });
+
+  const rules = reactive({
+    compensationFundName: [
+      { required: true, message: "El nombre es obligatorio", trigger: "blur" },
+      {
+        pattern: /^[a-zA-ZÁáÉéÍíÓóÚúÑñ\s]+$/,
+        message: "Solo letras y espacios",
+        trigger: "blur",
+      },
+    ],
+  });
+
+  const filterTableData = computed(() =>
+    familyCompensationFundList.value.filter(
+      (data) =>
+        !search.value ||
+        data.compensationFundName
+          .toLowerCase()
+          .includes(search.value.toLowerCase())
+    )
+  );
+
+  const paginatedData = computed(() => {
+    const start = (currentPage.value - 1) * pageSize.value;
+    return filterTableData.value.slice(start, start + pageSize.value);
+  });
+
+  const handlePageChange = (page: number) => {
+    currentPage.value = page;
+  };
+
+  const handleSizeChange = (size: number) => {
+    pageSize.value = size;
+    currentPage.value = 1;
+  };
+
+  const loadFamilyCompensationFund = async () => {
+    familyCompensationFundList.value =
+      (await familyCompensationFundStore.fetchFamilyCompensationFund()) || [];
+  };
+
+  const submitForm = async (formEl: FormInstance | undefined) => {
+    if (!formEl) return;
+    try {
+      await formEl.validate();
+
+      await familyCompensationFundStore.createFamilyCompensationFundRequest(
+        familyCompensationFundForm
+      );
+      await loadFamilyCompensationFund();
+
+      ElNotification({
+        title: "Éxito",
+        message: "FamilyCompensationFund creado correctamente",
+        type: "success",
+      });
+
+      familyCompensationFundForm.compensationFundName = "";
+    } catch (error) {
+      const errorMessage = error as string;
+      ElNotification({
+        title: "Error",
+        message: errorMessage,
+        type: "error",
+      });
+    }
+  };
+
+  watch([search, currentPage, pageSize], () => {
+    console.log("Filtros actualizados", {
+      search: search.value,
+      currentPage: currentPage.value,
+      pageSize: pageSize.value,
+    });
+  });
+
+  onMounted(async () => {
+    await loadFamilyCompensationFund();
+  });
+
+  return {
+    familyCompensationFundList,
+    isLoading,
+    search,
+    currentPage,
+    pageSize,
+    ruleFormRef,
+    rules,
+    paginatedData,
+    handlePageChange,
+    handleSizeChange,
+    submitForm,
+    familyCompensationFundForm,
+  };
+}
---
+++ src/presentation/views/PortalRRHH/manageEmployee/manageFamilyCompensationFundView.vue
@@ -0,0 +1,85 @@
+<template>
+  <el-card shadow="never">
+    <template #header>
+      <h2 class="text-xl text-gray-700 font-semibold">
+        Gestionar Fondos de compensación familiar
+      </h2>
+    </template>
+
+    <el-card shadow="never" class="mb-6">
+      <el-form
+        inline
+        ref="ruleFormRef"
+        :rules="rules"
+        :model="familyCompensationFundForm"
+      >
+        <el-form-item prop="compensationFundName" label="Nombre">
+          <el-input
+            v-model="familyCompensationFundForm.compensationFundName"
+            placeholder="Nombre"
+            clearable
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            :loading="isLoading"
+            type="primary"
+            @click="submitForm(ruleFormRef)"
+          >
+            Crear fondo
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <el-table :data="paginatedData" border class="w-full mb-4" stripe>
+      <el-table-column prop="id" label="ID" />
+      <el-table-column prop="compensationFundName" label="Nombre" />
+      <el-table-column prop="isActive" label="Estado">
+        <template #default="{ row }">
+          <el-tag :type="row.isActive ? 'success' : 'danger'">
+            {{ row.isActive ? "Activo" : "Inactivo" }}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="Acciones">
+        <template #default="scope">
+          <el-button size="small"> Editar </el-button>
+          <el-button :loading="isLoading" size="small" type="danger">
+            Desactivar
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- Paginación -->
+    <el-pagination
+      v-model:current-page="currentPage"
+      :page-size="pageSize"
+      :page-sizes="[10, 20, 50]"
+      layout="total, sizes, prev, pager, next"
+      :total="familyCompensationFundList.length"
+      @size-change="handleSizeChange"
+      @current-change="handlePageChange"
+    />
+  </el-card>
+</template>
+
+<script lang="ts" setup>
+import { useFamilyCompensationFundViewModel } from "@/presentation/viewmodels/familyCompensationFundViewModel";
+
+const {
+  familyCompensationFundList,
+  isLoading,
+  search,
+  currentPage,
+  pageSize,
+  ruleFormRef,
+  rules,
+  paginatedData,
+  handlePageChange,
+  handleSizeChange,
+  submitForm,
+  familyCompensationFundForm,
+} = useFamilyCompensationFundViewModel();
+</script>
