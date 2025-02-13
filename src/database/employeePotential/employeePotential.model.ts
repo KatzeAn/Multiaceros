9a59e34223fc528514e3672588dc13d4b60fd652
wilmaralfaro <wilmar.alfaro@techietalent.net>
2025-02-13 08:48:57 -0500

    feat: update getAllEmployeesPotential method to return an array of EmployeePotential

--- src/database/employeePotential/employeePotential.model.ts
+++ src/database/employeePotential/employeePotential.model.ts
@@ -6,8 +6,8 @@ export class EmployeePotentialModel extends EmployeePotentialRepository {
   createEmployeePotential(data: EmployeePotential): Promise<EmployeePotential> {
     return apiRequest<EmployeePotential>("post", "/api/EmployeePotential/Create", { data });
   }
-  getAllEmployeesPotential(): Promise<EmployeePotential> {
-    return apiRequest<EmployeePotential>("get", "/api/EmployeePotential/GetAllEmployees");
+  getAllEmployeesPotential(): Promise<EmployeePotential[]> {
+    return apiRequest<EmployeePotential[]>("get", "/api/EmployeePotential/GetAllEmployees");
   }
   updateEmployeePotential(data: EmployeePotential): Promise<EmployeePotential> {
     return apiRequest<EmployeePotential>("put", "/api/EmployeePotential/UpdateEmployeePotenjal", { data });
