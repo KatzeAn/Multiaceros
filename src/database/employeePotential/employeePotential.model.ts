343c6cec38fc472b4d6bfb5cdb4ad92692f67613
wilmaralfaro <wilmar.alfaro@techietalent.net>
2025-02-13 18:15:38 -0500

    refactor: simplify API request structure in EmployeePotentialModel

--- src/database/employeePotential/employeePotential.model.ts
+++ src/database/employeePotential/employeePotential.model.ts
@@ -4,15 +4,38 @@ import { apiRequest } from "@/presentation/api/axiosInstance";
 
 export class EmployeePotentialModel extends EmployeePotentialRepository {
   createEmployeePotential(data: EmployeePotential): Promise<EmployeePotential> {
-    return apiRequest<EmployeePotential>("post", "/EmployeePotential/CreateEmployeePotential", { data });
+    return apiRequest<EmployeePotential>(
+      "post",
+      "/EmployeePotential/CreateEmployeePotential",
+      {
+        documentType: data.documentType,
+        numberDocument: data.numberDocument,
+        firstName: data.firstName,
+        middleName: data.middleName,
+        surName: data.surName,
+        dateOfBirth: data.dateOfBirth,
+        email: data.email,
+        cellPhone: data.cellPhone,
+        jobPostingId: data.jobPostingId,
+      }
+    );
   }
   getAllEmployeesPotential(): Promise<EmployeePotential[]> {
-    return apiRequest<EmployeePotential[]>("get", "/EmployeePotential/GetAllEmployeesPotential");
+    return apiRequest<EmployeePotential[]>(
+      "get",
+      "/EmployeePotential/GetAllEmployeesPotential"
+    );
   }
   updateEmployeePotential(data: EmployeePotential): Promise<EmployeePotential> {
-    return apiRequest<EmployeePotential>("put", `/EmployeePotential/UpdateEmployeePotential/${data.numberDocument}`, { data });
+    return apiRequest<EmployeePotential>(
+      "put",
+      `/EmployeePotential/UpdateEmployeePotential/${data.numberDocument}`,
+      { data }
+    );
   }
-  getEmployeePotentialByDocument(numberDocument: number): Promise<EmployeePotential> {
+  getEmployeePotentialByDocument(
+    numberDocument: number
+  ): Promise<EmployeePotential> {
     return apiRequest<EmployeePotential>(
       "get",
       `/EmployeePotential/${numberDocument}`
