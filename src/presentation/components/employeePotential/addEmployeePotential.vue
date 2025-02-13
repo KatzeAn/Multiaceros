7ad253e8c005bcb74dc0f404ca5ba192101ca116
wilmaralfaro <wilmar.alfaro@techietalent.net>
2025-02-13 17:03:23 -0500

    feat: add form for employee potential submission and update error message in reset password viewmodel

--- src/presentation/components/employeePotential/addEmployeePotential.vue
+++ src/presentation/components/employeePotential/addEmployeePotential.vue
@@ -5,6 +5,55 @@
     title="Referir un candidato"
     top="6vh"
   >
+    <el-form
+      ref="ruleFormRef"
+      :model="employeePotentialForm"
+      :rules="rules"
+      label-width="auto"
+      class="w-full"
+      size="default"
+      label-position="top"
+      status-icon
+    >
+      <el-form-item label="Número de documento">
+        <el-input placeholder="Documento" v-model="employeePotentialForm.numberDocument" />
+      </el-form-item>
+
+      <el-form-item label="Primer Nombre">
+        <el-input placeholder="Primer nombre" v-model="employeePotentialForm.firstName" />
+      </el-form-item>
+
+      <el-form-item label="Segundo Nombre">
+        <el-input placeholder="Primer nombre" v-model="employeePotentialForm.middleName" />
+      </el-form-item>
+
+      <el-form-item label="Apellido">
+        <el-input placeholder="Apellido" v-model="employeePotentialForm.firstName" />
+      </el-form-item>
+
+      <el-form-item label="Fecha de nacimiento">
+      </el-form-item>
+
+      <el-form-item label="Correo eléctronico">
+        <el-input placeholder="Correo eléctronico" v-model="employeePotentialForm.email" />
+      </el-form-item>
+
+      <el-form-item label="Celular">
+        <el-input placeholder="Celular" v-model="employeePotentialForm.email" />
+      </el-form-item>
+
+      
+
+      <el-form-item class="mt-6">
+        <el-button
+          :loading="isLoading"
+          type="success"
+          @click="submitForm(ruleFormRef)"
+        >
+          Enviar Solicitud
+        </el-button>
+      </el-form-item>
+    </el-form>
   </el-dialog>
 </template>
 
@@ -17,6 +66,6 @@ const props = defineProps<{
 
 defineEmits(["update:dialog"]);
 
-const { loadEmployeePotentialByDocument, employeePotential } =
+const { ruleFormRef, rules, submitForm, isLoading, employeePotentialForm } =
   useEmployeePotentialViewModel();
 </script>
