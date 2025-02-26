d14c7aa169add1eb821bbec3eb0b0d9ddddd2fde
Angela <angela.alfaro1033@gmail.com>
2025-02-26 10:46:02 -0500

    actualizar perfil

--- src/database/user/UserProfile/UserProfile.model.ts
+++ src/database/user/UserProfile/UserProfile.model.ts
@@ -2,14 +2,26 @@ import type { UserProfile } from "@/domain/Interfaces/user/UserProfile.interface
 import { UserProfileRepository } from "@/domain/repository/user/UserProfile.repository";
 import axiosInstance from "@/presentation/api/axiosInstance";
 
-
 export class UserProfileModel extends UserProfileRepository {
   async getUserProfile(userId: string): Promise<UserProfile> {
     try {
       const response = await axiosInstance.get(`/User/GetUserProfile/${userId}`);
       return response.data as UserProfile;
     } catch (error) {
-        throw new Error("Unexpected error: " + error);
+      throw new Error("Unexpected error: " + error);
+    }
+  }
+
+  async updateUserProfile(userProfile: UserProfile): Promise<UserProfile> {
+    try {
+      const response = await axiosInstance.put("/User/UpdateUserProfile", userProfile, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return response.data as UserProfile;
+    } catch (error) {
+      throw new Error("Unexpected error: " + error);
     }
   }
 }
