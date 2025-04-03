a26b7669382b3af46c33823b1ad716a8be931a19
Angela <angela.alfaro1033@gmail.com>
2025-04-03 12:15:09 -0500

    boton salir

--- src/presentation/views/Applications.vue
+++ src/presentation/views/Applications.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app" class="app flex h-screen bg-cover bg-center relative" style="background-image: url('/src/presentation/assets/historia.png');">
+    
+    <button @click="logout" class="absolute top-4 left-4 bg-gray-800 text-white p-2 rounded-full shadow-md flex items-center gap-2">
+      <span class="material-symbols-outlined text-3xl">logout</span>
+      <span class="text-white text-lg">{{ t('logout') }}</span>
+    </button>
+
     <button @click="showSettings = !showSettings" class="absolute top-4 right-4 bg-gray-800 text-white p-2 rounded-full shadow-md">
       <span class="material-symbols-outlined text-3xl">settings</span>
     </button>
 
-    <div v-if="showSettings" class="absolute top-16 right-4   rounded-lg shadow-md">
+    <div v-if="showSettings" class="absolute top-16 right-4 rounded-lg shadow-md">
       <LanguageSwitcher />
     </div>
 
@@ -12,9 +18,8 @@
       <div v-if="!currentSection" class="text-left mb-8">
         <p class="text-white text-sm uppercase tracking-widest">{{ t('work_hard_play_hard') }}</p>
         <h1 class="text-5xl md:text-7xl font-bold text-white leading-tight">
-  {{ t("work_with") }} <br /> {{ t("us") }}
-</h1>
-
+          {{ t("work_with") }} <br /> {{ t("us") }}
+        </h1>
       </div>
 
       <div v-if="!currentSection" class="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full max-w-2xl">
@@ -31,7 +36,7 @@
         </div>
       </div>
 
-      <div v-if="currentSection === 'sobreNosotros'" class="mt-10 w-full ">
+      <div v-if="currentSection === 'sobreNosotros'" class="mt-10 w-full">
         <button @click="currentSection = null" class="text-white underline mb-4">&larr; {{ t('go_back') }}</button>
         <AboutUs />
       </div>
@@ -54,10 +59,16 @@ import AboutUs from '../components/ApplicationsComponents/AboutUs.vue';
 import LanguageSwitcher from "@/presentation/components/LanguageSwitcher.vue";
 import { useI18n } from "vue-i18n";
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 const { t } = useI18n();
 const currentSection = ref(null);
 const showSettings = ref(false);
+const router = useRouter();
+
+const logout = () => {
+  router.push('/'); 
+};
 </script>
 
 <style scoped>
